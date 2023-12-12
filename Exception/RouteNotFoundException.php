e2f2f746db97b143704b2ced12c79bf9a128d16f
tuancanhit <tuancanhit@gmail.com>
2023-12-12 10:25:19 +0700

    Init base

---
+++ Exception/RouteNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Will, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace William\Core\Exception;
+
+/**
+ * Class RouteNotFoundException
+ */
+class RouteNotFoundException extends \Exception
+{
+
+}
